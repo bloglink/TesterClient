a611aaf40edaf6bbb76986d4bd75f624fe2a9081
zhaonanlin <zzznanlin@126.com>
2017-08-28 17:03:41 +0800

    格式:二维码生成界面格式修正

--- conf_qrencode.cpp
+++ conf_qrencode.cpp
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright [2017]   <青岛艾普智能仪器有限公司>
+ * All rights reserved.
+ *
+ * version:     0.1
+ * author:      zhaonanlin
+ * brief:       二维码生成
+*******************************************************************************/
 #include "conf_qrencode.h"
 
 ConfQrenCode::ConfQrenCode(QWidget *parent) : QWidget(parent)
@@ -14,9 +22,8 @@ ConfQrenCode::~ConfQrenCode()
 void ConfQrenCode::generateString(QString str)
 {
     string = str;
-    if(qr != NULL)
+    if (qr != NULL)
         QRcode_free(qr);
-
     qr = QRcode_encodeString(string.toStdString().c_str(),
                              1,
                              QR_ECLEVEL_L,
@@ -27,22 +34,17 @@ void ConfQrenCode::generateString(QString str)
 
 bool ConfQrenCode::saveImage(QString fileName, int size)
 {
-    if(size != 0 && !fileName.isEmpty())
-    {
+    if (size != 0 && !fileName.isEmpty()) {
         QImage image(size, size, QImage::Format_Mono);
         QPainter painter(&image);
         QColor background(Qt::white);
         painter.setBrush(background);
         painter.setPen(Qt::NoPen);
         painter.drawRect(0, 0, size, size);
-        if(qr != NULL)
-        {
+        if (qr != NULL)
             draw(painter, size, size);
-        }
         return image.save(fileName);
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
@@ -54,13 +56,10 @@ void ConfQrenCode::draw(QPainter &painter, int width, int height)
     const int qr_width = qr->width > 0 ? qr->width : 1;
     double scale_x = width / qr_width;
     double scale_y = height / qr_width;
-    for( int y = 0; y < qr_width; y ++)
-    {
-        for(int x = 0; x < qr_width; x++)
-        {
+    for (int y = 0; y < qr_width; y++) {
+        for (int x = 0; x < qr_width; x++) {
             unsigned char b = qr->data[y * qr_width + x];
-            if(b & 0x01)
-            {
+            if (b & 0x01) {
                 QRectF r(x * scale_x, y * scale_y, scale_x, scale_y);
                 painter.drawRects(&r, 1);
             }
@@ -68,29 +67,25 @@ void ConfQrenCode::draw(QPainter &painter, int width, int height)
     }
 }
 
-void ConfQrenCode::paintEvent(QPaintEvent *)
+void ConfQrenCode::paintEvent(QPaintEvent *e)
 {
     QPainter painter(this);
     QColor background(Qt::white);
     painter.setBrush(background);
     painter.setPen(Qt::NoPen);
     painter.drawRect(0, 0, width(), height());
-    if(qr != NULL)
-    {
+    if (qr != NULL)
         draw(painter, width(), height());
-    }
+    e->accept();
 }
 
 QSize ConfQrenCode::sizeHint() const
 {
     QSize s;
-    if(qr != NULL)
-    {
+    if (qr != NULL) {
         int qr_width = qr->width > 0 ? qr->width : 1;
         s = QSize(qr_width * 4, qr_width * 4);
-    }
-    else
-    {
+    } else {
         s = QSize(50, 50);
     }
     return s;
@@ -99,13 +94,10 @@ QSize ConfQrenCode::sizeHint() const
 QSize ConfQrenCode::minimumSizeHint() const
 {
     QSize s;
-    if(qr != NULL)
-    {
+    if (qr != NULL) {
         int qr_width = qr->width > 0 ? qr->width : 1;
         s = QSize(qr_width, qr_width);
-    }
-    else
-    {
+    } else {
         s = QSize(50, 50);
     }
     return s;
--- conf_qrencode.h
+++ conf_qrencode.h
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Copyright [2017]   <青岛艾普智能仪器有限公司>
+ * All rights reserved.
+ *
+ * version:     0.1
+ * author:      zhaonanlin
+ * brief:       二维码生成
+*******************************************************************************/
 #ifndef CONFQRENCODE_H
 #define CONFQRENCODE_H
 
+
 #include <QWidget>
 #include <QPainter>
+#include <QPaintEvent>
 #include "qrencode.h"
+#include "string"
 
 class ConfQrenCode : public QWidget
 {
@@ -12,14 +23,14 @@ public:
     explicit ConfQrenCode(QWidget *parent = 0);
     ~ConfQrenCode();
     void generateString(QString str);
-    bool saveImage(QString fileName, int size=250);
+    bool saveImage(QString fileName, int size = 250);
 private:
     void draw(QPainter &painter, int width, int height);
     QString string;
     QRcode *qr;
 
 protected:
-    void paintEvent(QPaintEvent *);
+    virtual void paintEvent(QPaintEvent *e);
     QSize sizeHint() const;
     QSize minimumSizeHint() const;
 };
